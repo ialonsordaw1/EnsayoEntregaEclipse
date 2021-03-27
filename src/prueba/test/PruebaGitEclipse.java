543c62f629c77a77bbd771ee4fab05f0944639d0
ialonsordaw <ialonsordaw1@gmail.com>
2021-03-27 13:02:25 +0100

    Añadido comentario a la clase PruebaGitEclipse

--- src/prueba/test/PruebaGitEclipse.java
+++ src/prueba/test/PruebaGitEclipse.java
@@ -1,7 +1,11 @@
 package prueba.test;
 
 import prueba.modelo.Saludo;
-
+/*
+ * Punto de entrada a la aplicación
+ * @author Iñigo, David Burguete, Javier
+ * 
+ * */
 public class PruebaGitEclipse {
 
 	public static void main(String[] args) {
