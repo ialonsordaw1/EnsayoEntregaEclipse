5ad6a5730ae8f982828f5c26506aa496d47c038a
dburguetegdaw1 <dburguetegdaw1@gmail.com>
2021-03-30 20:58:37 +0200

    Saludo a Isabel

--- src/prueba/test/PruebaGitEclipse.java
+++ src/prueba/test/PruebaGitEclipse.java
@@ -12,6 +12,7 @@ public class PruebaGitEclipse {
 		Saludo saludo = new Saludo();
 		System.out.println(saludo.saludar("Juan"));
 		System.out.println(saludo.saludar("Javier"));
+		System.out.println(saludo.saludar("Isabel"));
 
 	}
 
